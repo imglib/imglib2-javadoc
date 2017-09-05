822012190a3e1b6ac8b29d498ac22339156ecaca
Curtis Rueden <ctrueden@wisc.edu>
2017-09-05 09:40:15 -0500

    Add Travis CI configuration

---
+++ .travis/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
+sh travis-javadoc.sh ImgLib2 $encrypted_56a73366b711_key $encrypted_56a73366b711_iv
